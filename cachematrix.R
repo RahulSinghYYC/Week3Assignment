3aed9483ecce741a0c48538f8204030334c840d9
Rahul Singh <rahul2ksingh@gmail.com>
2016-07-24 22:04:02 -0600

    Week3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##MCreats matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+
+        inv <- NULL
+        set <- function (y){
+                x <<- y
+                inv <<- NULL
+                
+        }
+        #gets the matrix not the inverse
+        get <- function()x
+        
+        #set the inverse
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set,get=get, 
+             setinverse = setinverse,
+             getinverse=getinverse)
 
 }
 
@@ -12,4 +28,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        
+        if(!is.null(inv)) {
+                message("Getting cached matrix")
+                return(inv)
+        }
+        data <-x$get()
+        
+        inv <- solve(data, ...)
+        
+        x$setinverse(inv)
+        
+        inv
 }
+
+
